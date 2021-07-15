4893f2275a25b2eb0808af16e235b061a8185d8a
DongPhamVan <dongphamvan.bkap@gmail.com>
2021-07-15 23:19:04 +0700

    get and displace dataframes

--- src/sales/utils.py
+++ src/sales/utils.py
@@ -1,5 +1,15 @@
 import uuid
+from customers.models import Customer
+from profiles.models import Profile
 
 def generate_code():
     code = str(uuid.uuid4()).replace('-', '').upper()[:12]
     return code
+
+def get_customer_from_id(val):
+    customer = Customer.objects.get(id=val)
+    return customer
+
+def get_salesman_from_id(val):
+    profile = Profile.objects.get(id=val)
+    return profile.user.username
--- src/sales/views.py
+++ src/sales/views.py
@@ -3,20 +3,42 @@ from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
 import pandas as pd
+from  .utils import get_customer_from_id, get_salesman_from_id
 
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        print(chart_type, date_from, date_to)
-        qs = Sale.objects.filter(created__date=date_from)
-        df_1 = pd.DataFrame(qs.values())
-        print(df_1)
+        sales_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        if len(sales_qs) > 0:
+            sales_df = pd.DataFrame(sales_qs.values())
+            sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
+            sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
+            position_data = []
+            for sale in sales_qs:
+                for pos in sale.get_positions():
+                    obj ={
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                        'sales_id': pos.get_sales_id(),
+
+                    }        
+                    position_data.append(obj)
+            positions_df = pd.DataFrame(position_data)
+            sales_df = sales_df.to_html()
+            positions_df = positions_df.to_html()
+        else:
+            print('no_data')
 
     context = {
         'form': form,
+        'sales_df': sales_df,
+        'positions_df': positions_df,
     }
     return render(request, 'sales/home.html', context)
 
